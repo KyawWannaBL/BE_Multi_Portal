ecdffac893d2d57b6ff09fd3bf3efd5cb6c532ee
Kyaw Wanna <mgkyawwanna@gmail.com>
2026-03-05 18:23:03 +0630

    fix: remove environment validator blocker from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,15 @@ import "./index.css";
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
-import ConfigFault from "@/components/ConfigFault";
-import { validateEnv } from "@/lib/env";
 
-const env = validateEnv();
 const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <ErrorBoundary>
-      {env.ok ? (
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      ) : (
-        <ConfigFault missing={env.missing} />
-      )}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
